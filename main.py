ed011a9b0381e7fe5c1efe5dc0d12abd1c538b34
Max Knutson <mknutson@imsa.edu>
2021-01-05 14:12:29 -0500

    Ping function working

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from discord.ext import commands
-prefix = ">"
+import random
+prefix = '-'
 bot = commands.Bot(command_prefix=prefix)
 
 @bot.event
 async def on_ready():
-    print("UIUC COVID DATA BOT IS ONLINE")
-
-@bot.event
-async def on_message(message):
-    message.content.lower()
-    if message.author == bot.user:
-        return
-    if message.content.startswith("deez"):
-        await message.channel.send("Nuts!")
+    print('UIUC COVID DATA BOT IS ONLINE')
 
 @bot.command(name='ping')
 async def ping(ctx):
@@ -21,6 +14,6 @@ async def ping(ctx):
     '''
 
     latency = bot.latency
-    await ctx.send(latency)
+    await ctx.send('Pong!! jk.\n\nPing is: ' + str(latency))
 
-bot.run('Nzk1NzEwNDU2Mjk1MTk0NjQ0.X_NVHA.I9E22zdenClo_9oI4dUW9Haopio')
+bot.run('Nzk1NzEwNDU2Mjk1MTk0NjQ0.X_NVHA.ezGXi9wHlVXMFjfY2Uk-WMJlVXk')
