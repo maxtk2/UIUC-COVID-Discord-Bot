ba83d25ad00e686f2868c848790b036d8dfd29c1
ThailerL <thailer.lietz@yahoo.com>
2021-01-07 03:43:27 -0600

    Simplify web scraping

--- data_scraper.py
+++ data_scraper.py
@@ -12,12 +12,7 @@ class DataScraper:
 
     def scrape(self):
         browser = webdriver.Chrome(chrome_options=self.options)
-        home_page = browser.get('https://covid19.illinois.edu/on-campus-covid-19-testing-data-dashboard/')
-        home_html = browser.page_source
-        homeSoup = BeautifulSoup(home_html, 'lxml')
-        link = homeSoup.find('a', href=True, text='View the Testing Data Dashboard here')
-        url = link.get('href')
-        data_page = browser.get(url)
+        data_page = browser.get('https://go.illinois.edu/COVIDTestingData')
         time.sleep(4) # This is bad, but was the only way I could get the page to load before scraping.
         data_html = browser.page_source
         dataSoup = BeautifulSoup(data_html, 'lxml')
